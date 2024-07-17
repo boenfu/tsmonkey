d05a4080aad807c2616cbff580539050ca1a4d8e
boenfu <1462957021@qq.com>
2024-07-18 00:08:34 +0800

    lexer: identifier

--- lexer.ts
+++ lexer.ts
@@ -24,26 +24,38 @@ declare enum TokenType {
   LET = 'LET',
 }
 
+type Letter = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z' | 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M' | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z' | '_'
+
 interface Token<TType extends TokenType, TLiteral> {
   type: TType
   literal: TLiteral
 }
 
 type Lexer<TCode extends string> = TCode extends `${infer TCh}${infer TRest}`
-  ? [
-      // 空白字符跳过
-      ...(TCh extends ' '
-        ? []
-        : Extract<TokenType, TCh> extends never
+  // 是字母/下划线
+  ? TCh extends Letter
+    ? ReadIdentifier<TCode> extends [infer TIdentity, infer TRest extends string]
+      // 遍历读取标识符，剩余的继续递归
+      ? [Token<TokenType.IDENT, TIdentity>, ...Lexer<TRest>]
+      : never
+    : [
+        ...(TCh extends ' '
+          // 空白字符跳过
           ? []
-          // 能直接匹配上的 token
-          : [Token<Extract<TokenType, TCh>, TCh>]),
-      ... TRest extends ''
-        // 结束标记
-        ? [Token<TokenType.EOF, ''>]
-        // 递归解析
-        : Lexer<TRest>,
-    ]
+          : Extract<TokenType, TCh> extends never
+            ? []
+            // 能直接匹配上的 token
+            : [Token<Extract<TokenType, TCh>, TCh>]),
+        ... TRest extends ''
+          // 结束标记
+          ? [Token<TokenType.EOF, ''>]
+          // 递归解析
+          : Lexer<TRest>,
+      ]
   : []
 
-type _L1 = Lexer<'=  1 3 )'>
+type ReadIdentifier<TCode extends string, TOutput extends string = ''> = TCode extends `${infer TCh extends Letter}${infer TRest}`
+  ? ReadIdentifier<TRest, `${TOutput}${TCh}`>
+  : [TOutput, TCode]
+
+type _L1 = Lexer<'= abc _oo CC (); '>
