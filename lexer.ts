018e3f60f2d3ef9afc2d7b796009b3c1a8303ce1
boenfu <1462957021@qq.com>
2024-07-18 13:59:59 +0800

    lexer: keyword & digit

--- lexer.ts
+++ lexer.ts
@@ -9,6 +9,20 @@ declare enum TokenType {
   // 运算符
   ASSIGN = '=',
   PLUS = '+',
+  MINUS = '-',
+  BAND = '!',
+  ASTERISK = '*',
+  SLASH = '/',
+
+  //   EQ = '==',
+  //   NEQ = '!=',
+
+  LT = '<',
+  GT = '>',
+  //   LE = '<=',
+  //   GE = '>=',
+  //   AND = '&&',
+  //   OR = '||',
 
   // 分隔符
   COMMA = ',',
@@ -20,42 +34,73 @@ declare enum TokenType {
   RBRACE = '}',
 
   // 关键字
-  FUNCTION = 'FUNCTION',
-  LET = 'LET',
+  FUNCTION = 'function',
+  LET = 'let',
+  VAR = 'var',
+  TRUE = 'true',
+  FALSE = 'false',
+  IF = 'if',
+  ELSE = 'else',
+  RETURN = 'return',
+  //   WHILE = 'while',
+  //   FOR = 'for',
+  //   CONTINUE = 'continue',
+  //   BREAK = 'break',
+  //   NEW = 'new',
+  //   THIS = 'this',
+  //   NULL = 'null',
 }
 
-type Letter = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z' | 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M' | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z' | '_'
+type Letter = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z' | 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M' | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z' | '_' | '$'
+type Digit = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
 
 interface Token<TType extends TokenType, TLiteral> {
   type: TType
   literal: TLiteral
 }
 
+type WhiteSpace = ' ' | '\n' | '\t' | '\r'
+
 type Lexer<TCode extends string> = TCode extends `${infer TCh}${infer TRest}`
-  // 是字母/下划线
+  // 是字母/下划线/美元符
   ? TCh extends Letter
     ? ReadIdentifier<TCode> extends [infer TIdentity, infer TRest extends string]
       // 遍历读取标识符，剩余的继续递归
-      ? [Token<TokenType.IDENT, TIdentity>, ...Lexer<TRest>]
+      ? [Token<Extract<TokenType, TIdentity> extends never ? TokenType.IDENT : Extract<TokenType, TIdentity>, TIdentity>, ...Lexer<TRest>]
       : never
-    : [
-        ...(TCh extends ' '
-          // 空白字符跳过
-          ? []
-          : Extract<TokenType, TCh> extends never
+
+    : TCh extends Digit
+      ? ReadDigit<TCode> extends [infer TIdentity, infer TRest extends string]
+        // 遍历读取标识符，剩余的继续递归
+        ? [Token<TokenType.INT, TIdentity>, ...Lexer<TRest>]
+        : never
+      : [
+          ...(TCh extends WhiteSpace
+          // 空白/换行字符跳过
             ? []
+            : Extract<TokenType, TCh> extends never
+              ? [TCh]
             // 能直接匹配上的 token
-            : [Token<Extract<TokenType, TCh>, TCh>]),
-        ... TRest extends ''
+              : [Token<Extract<TokenType, TCh>, TCh>]),
+          ... TRest extends ''
           // 结束标记
-          ? [Token<TokenType.EOF, ''>]
+            ? [Token<TokenType.EOF, ''>]
           // 递归解析
-          : Lexer<TRest>,
-      ]
+            : Lexer<TRest>,
+        ]
   : []
 
-type ReadIdentifier<TCode extends string, TOutput extends string = ''> = TCode extends `${infer TCh extends Letter}${infer TRest}`
+type ReadIdentifier<TCode extends string, TOutput extends string = ''> = TCode extends
+// 允许标识符中非开头出现数字
+`${infer TCh extends (TOutput extends '' ? Letter : Letter | Digit)}${infer TRest}`
   ? ReadIdentifier<TRest, `${TOutput}${TCh}`>
   : [TOutput, TCode]
 
-type _L1 = Lexer<'= abc _oo CC (); '>
+type ReadDigit<TCode extends string, TOutput extends string = ''> = TCode extends
+  `${infer TCh extends Digit}${infer TRest}`
+  ? ReadDigit<TRest, `${TOutput}${TCh}`>
+  : [TOutput, TCode]
+
+type _L1 = Lexer<`
+let 5g = g5(); 
+`>
