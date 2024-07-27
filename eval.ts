e7b678e01488f086bee59ab585a5aa778557b07d
boenfu <1462957021@qq.com>
2024-07-27 22:44:40 +0800

    eval: infix expression

--- eval.ts
+++ eval.ts
@@ -2,6 +2,7 @@ import type { Lexer, TokenType } from './lexer'
 import type {
   BooleanLiteral,
   ExpressionStatement,
+  InfixExpression,
   IntegerLiteral,
   Node,
   Parser,
@@ -9,7 +10,7 @@ import type {
   Program,
   Statement,
 } from './parser'
-import type { IsTruthy } from './utils'
+import type { Divide, EQ, GT, IsTruthy, LT, Minus, Multiply, NEQ, Plus } from './utils'
 
 export type Eval<TNode extends Node> = {
   Program: TNode extends Program<infer TStatements> ? EvalStatements<TStatements> : never
@@ -21,7 +22,7 @@ export type Eval<TNode extends Node> = {
   IntegerLiteral: TNode extends IntegerLiteral<infer TValue> ? TValue : never
   BooleanLiteral: TNode extends BooleanLiteral<infer TValue> ? TValue : never
   PrefixExpression: TNode extends PrefixExpression<infer TPrefix, infer TRight> ? EvalPrefixExpression<TPrefix, Eval<TRight>> : never
-  InfixExpression: 7
+  InfixExpression: TNode extends InfixExpression<infer TInfixTokenType, infer TLeft, infer TRight> ? EvalInfixExpression<TInfixTokenType, Eval<TLeft>, Eval<TRight>> : never
   IfExpression: 8
   FunctionLiteral: 9
   CallExpression: 10
@@ -42,5 +43,19 @@ type EvalPrefixExpression<TPrefixTokenType extends TokenType, TValue> = {
   [TokenType.BAND]: IsTruthy<TValue> extends true ? false : true
 } extends { [T in TPrefixTokenType]: infer TR } ? TR : never
 
+type EvalInfixExpression<TInfixTokenType extends TokenType, TLeft, TRight> = {
+  [TokenType.PLUS]: Plus<TLeft & number, TRight & number>
+  [TokenType.MINUS]: Minus<TLeft & number, TRight & number>
+  [TokenType.SLASH]: Divide<TLeft & number, TRight & number>
+  [TokenType.ASTERISK]: Multiply<TLeft & number, TRight & number>
+  [TokenType.EQ]: EQ<TLeft, TRight>
+  [TokenType.NEQ]: NEQ<TLeft, TRight>
+  [TokenType.LT]: LT<TLeft & number, TRight & number>
+  [TokenType.GT]: GT<TLeft & number, TRight & number>
+  [TokenType.LPAREN]: never
+} extends { [T in TInfixTokenType]: infer TR } ? TR : never
+
 type _E1 = Eval<Parser<Lexer<'a + b'>>>
 type _E2 = Eval<Parser<Lexer<'!!!!!!!!!0'>>>
+type _E3 = Eval<Parser<Lexer<'(3 + 2) * 5 == 25'>>>
+type _E4 = Eval<Parser<Lexer<'(3 > 2) != (2 > 3)'>>>
