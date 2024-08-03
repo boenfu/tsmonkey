443785419ac57a3e40c9f5aea3c31f84cc1e620d
boenfu <1462957021@qq.com>
2024-08-03 12:21:49 +0800

    chore: example code style

--- README.md
+++ README.md
@@ -64,7 +64,7 @@ function fibonacci(n) {
   if (n < 2) { return 0 }
   if (n == 2) { return 1 }
 
-  return fibonacci(n-1) + fibonacci(n - 2)
+  return fibonacci(n - 1) + fibonacci(n - 2)
 }
 
 fibonacci(6)
--- example.ts
+++ example.ts
@@ -117,7 +117,7 @@ function fibonacci(n) {
   if (n < 2) { return 0 } 
   if (n == 2) { return 1 }
 
-  return fibonacci(n-1) + fibonacci(n - 2)
+  return fibonacci(n - 1) + fibonacci(n - 2)
 }
 
 fibonacci(5)
@@ -128,7 +128,7 @@ function fibonacci(n) {
   if (n < 2) { return 0 } 
   if (n == 2) { return 1 }
 
-  return fibonacci(n-1) + fibonacci(n - 2)
+  return fibonacci(n - 1) + fibonacci(n - 2)
 }
 
 fibonacci(6)
